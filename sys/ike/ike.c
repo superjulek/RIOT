1af321eed8671164bcc53ad5e0311595abcc3848
Juliusz Neuman <01132997@pw.edu.pl>
2022-05-28 12:26:49 +0200

    Fixy

--- sys/ike/ike.c
+++ sys/ike/ike.c
@@ -23,7 +23,6 @@
 #include <stdio.h>
 #include <stdint.h>
 
-
 #include "net/gnrc/ipv6.h"
 #include "net/gnrc/netif.h"
 #include "net/gnrc/netif/hdr.h"
@@ -38,8 +37,8 @@
 #define IKE_NONCE_I_LEN 16
 #define MSG_BUF_LEN 1280
 
-
-typedef struct {
+typedef struct
+{
     ike_state_t state;
     uint64_t ike_spi_i;
     uint64_t ike_spi_r;
@@ -61,7 +60,6 @@ static int _init_context(void);
 static int _reset_context(void);
 static int _build_init_i(char *msg, size_t *msg_len);
 static int _parse_init_r(char *msg, size_t msg_len);
-
 
 int ike_init(char *addr_str)
 {
@@ -98,16 +96,14 @@ int ike_init(char *addr_str)
         // TODO: retry
         return -1;
     }
-    if (_parse_init_r(data_out, len) < 0)
+    if (_parse_init_r(data_in, len) < 0)
     {
         puts("Parsing IKE INIT message failed");
         return -1;
     }
 
-
     return 0;
 }
-
 
 static int _send_data(char *addr_str, char *data, size_t datalen)
 {
@@ -126,7 +122,8 @@ static int _send_data(char *addr_str, char *data, size_t datalen)
     unsigned payload_size;
     /* allocate payload */
     payload = gnrc_pktbuf_add(NULL, data, datalen, GNRC_NETTYPE_UNDEF);
-    if (payload == NULL) {
+    if (payload == NULL)
+    {
         puts("Error: unable to copy data to packet buffer");
         return -1;
     }
@@ -134,21 +131,24 @@ static int _send_data(char *addr_str, char *data, size_t datalen)
     payload_size = (unsigned)payload->size;
     /* allocate UDP header, set source port := destination port */
     udp = gnrc_udp_hdr_build(payload, port, port);
-    if (udp == NULL) {
+    if (udp == NULL)
+    {
         puts("Error: unable to allocate UDP header");
         gnrc_pktbuf_release(payload);
         return -1;
     }
     /* allocate IPv6 header */
     ip = gnrc_ipv6_hdr_build(udp, NULL, &addr);
-    if (ip == NULL) {
+    if (ip == NULL)
+    {
         puts("Error: unable to allocate IPv6 header");
         gnrc_pktbuf_release(udp);
         return -1;
     }
     /* send packet */
     if (!gnrc_netapi_dispatch_send(GNRC_NETTYPE_UDP,
-                                   GNRC_NETREG_DEMUX_CTX_ALL, ip)) {
+                                   GNRC_NETREG_DEMUX_CTX_ALL, ip))
+    {
         puts("Error: unable to locate UDP thread");
         gnrc_pktbuf_release(ip);
         return -1;
@@ -166,51 +166,56 @@ static void _process_incoming(gnrc_pktsnip_t *pkt, chunk_t *recv_chunk)
     int snips = 0;
     int size = 0;
     gnrc_pktsnip_t *snip = pkt;
-    //int address_ok = false;
-    //uint64_t src_port = 0;
+    // int address_ok = false;
+    // uint64_t src_port = 0;
 
-    while (snip != NULL) {
+    while (snip != NULL)
+    {
         printf("~~ SNIP %2i - size: %3u byte, type: ", snips, (unsigned int)snip->size);
 
         size_t hdr_len = 0;
 
         switch (snip->type)
         {
-            case GNRC_NETTYPE_UDP:
-                printf("NETTYPE_UDP (%i)\n", snip->type);
-                if (IS_USED(MODULE_UDP)) {
-                    udp_hdr_print(snip->data);
-                    hdr_len = sizeof(udp_hdr_t);
+        case GNRC_NETTYPE_UDP:
+            printf("NETTYPE_UDP (%i)\n", snip->type);
+            if (IS_USED(MODULE_UDP))
+            {
+                udp_hdr_print(snip->data);
+                hdr_len = sizeof(udp_hdr_t);
+            }
+            break;
+        case GNRC_NETTYPE_IPV6:
+            printf("NETTYPE_IPV6 (%i)\n", snip->type);
+            if (IS_USED(MODULE_IPV6_HDR))
+            {
+                ipv6_hdr_print(snip->data);
+                hdr_len = sizeof(ipv6_hdr_t);
+            }
+            break;
+        case GNRC_NETTYPE_NETIF:
+            printf("NETTYPE_NETIF (%i)\n", snip->type);
+            if (IS_USED(MODULE_GNRC_NETIF_HDR))
+            {
+                gnrc_netif_hdr_print(snip->data);
+                hdr_len = snip->size;
+            }
+            break;
+        case GNRC_NETTYPE_UNDEF:
+            printf("NETTYPE_UNDEF (%i)\n", snip->type);
+            if (hdr_len < snip->size)
+            {
+                size_t data_size = snip->size - hdr_len;
+                od_hex_dump(((uint8_t *)snip->data) + hdr_len, data_size, OD_WIDTH_DEFAULT);
+                if (data_size <= MSG_BUF_LEN)
+                {
+                    memcpy(recv_chunk->ptr, snip->data, data_size);
+                    recv_chunk->len = data_size;
                 }
-                break;
-            case GNRC_NETTYPE_IPV6:
-                printf("NETTYPE_IPV6 (%i)\n", snip->type);
-                if (IS_USED(MODULE_IPV6_HDR)) {
-                    ipv6_hdr_print(snip->data);
-                    hdr_len = sizeof(ipv6_hdr_t);
-                }
-                break;
-            case GNRC_NETTYPE_NETIF:
-                printf("NETTYPE_NETIF (%i)\n", snip->type);
-                if (IS_USED(MODULE_GNRC_NETIF_HDR)) {
-                    gnrc_netif_hdr_print(snip->data);
-                    hdr_len = snip->size;
-                }
-                break;
-            case GNRC_NETTYPE_UNDEF:
-                printf("NETTYPE_UNDEF (%i)\n", snip->type);
-                if (hdr_len < snip->size) {
-                    size_t data_size = snip->size - hdr_len;
-                    od_hex_dump(((uint8_t *)snip->data) + hdr_len, data_size, OD_WIDTH_DEFAULT);
-                    if (data_size <= MSG_BUF_LEN)
-                    {
-                        memcpy(recv_chunk->ptr, snip->data, data_size);
-                        recv_chunk->len = data_size;
-                    }
-                }
-                break;
-            default:
-                break;
+            }
+            break;
+        default:
+            break;
         }
 
         ++snips;
@@ -222,7 +227,7 @@ static void _process_incoming(gnrc_pktsnip_t *pkt, chunk_t *recv_chunk)
 
 static void *_eventloop(void *arg)
 {
-    chunk_t *recv_chunk = (chunk_t *) arg;
+    chunk_t *recv_chunk = (chunk_t *)arg;
     msg_t msg, reply;
     msg_t server_queue[1];
 
@@ -232,21 +237,23 @@ static void *_eventloop(void *arg)
     reply.content.value = (uint32_t)(-ENOTSUP);
     reply.type = GNRC_NETAPI_MSG_TYPE_ACK;
 
-    while (1) {
+    while (1)
+    {
         msg_receive(&msg);
 
-        switch (msg.type) {
-            case GNRC_NETAPI_MSG_TYPE_RCV:
-                printf("Packets received");
-                _process_incoming(msg.content.ptr, recv_chunk);
-                gnrc_pktbuf_release(msg.content.ptr);
-                break;
-            case GNRC_NETAPI_MSG_TYPE_GET:
-            case GNRC_NETAPI_MSG_TYPE_SET:
-                msg_reply(&msg, &reply);
-                break;
-            default:
-                break;
+        switch (msg.type)
+        {
+        case GNRC_NETAPI_MSG_TYPE_RCV:
+            printf("Packets received");
+            _process_incoming(msg.content.ptr, recv_chunk);
+            gnrc_pktbuf_release(msg.content.ptr);
+            break;
+        case GNRC_NETAPI_MSG_TYPE_GET:
+        case GNRC_NETAPI_MSG_TYPE_SET:
+            msg_reply(&msg, &reply);
+            break;
+        default:
+            break;
         }
     }
 
@@ -256,20 +263,18 @@ static void *_eventloop(void *arg)
 
 static int _receive_data(char *addr_str, char *data, size_t *datalen, uint32_t timeout)
 {
-    (void )addr_str;
-    (void) data;
-    (void) datalen;
+    (void)addr_str;
     uint16_t port = 500;
     chunk_t receive_chunk = {
         .ptr = data,
     };
-    
+
     gnrc_netreg_entry_t server = GNRC_NETREG_ENTRY_INIT_PID(0, KERNEL_PID_UNDEF);
     kernel_pid_t server_pid = KERNEL_PID_UNDEF;
     static char server_stack[THREAD_STACKSIZE_MAIN];
-    
+
     server_pid = thread_create(server_stack, sizeof(server_stack), THREAD_PRIORITY_MAIN - 1,
-        THREAD_CREATE_STACKTEST, _eventloop, (void*)&receive_chunk, "UDP server");
+                               THREAD_CREATE_STACKTEST, _eventloop, (void *)&receive_chunk, "UDP server");
     if (server_pid <= KERNEL_PID_UNDEF)
     {
         puts("Error: can not start server thread");
@@ -294,13 +299,11 @@ static int _receive_data(char *addr_str, char *data, size_t *datalen, uint32_t t
     {
         puts("Received data:");
         od_hex_dump(receive_chunk.ptr, receive_chunk.len, 0);
+        *datalen = receive_chunk.len;
+        return 0;
     }
-    return 0;
+    return -ENOMSG;
 }
-
-
-
-
 
 static int _reset_context(void)
 {
@@ -363,21 +366,24 @@ static int _build_init_i(char *msg, size_t *msg_len)
 
     /* Construct SA payload */
     error = build_sa_payload(msg + cur_len, MSG_BUF_LEN - cur_len, &new_len, IKE_PT_NONCE, IKE_PROTO_IKE, ike_ctx.ike_encr,
-        ike_ctx.ike_prf, ike_ctx.ike_integ, ike_ctx.ike_dh, ike_ctx.ike_esn, ike_ctx.ike_key_size, empty_chunk);
-    if (error < 0) return error;
+                             ike_ctx.ike_prf, ike_ctx.ike_integ, ike_ctx.ike_dh, ike_ctx.ike_esn, ike_ctx.ike_key_size, empty_chunk);
+    if (error < 0)
+        return error;
     cur_len += new_len;
 
     /* Construct Nonce payload */
     error = build_nonce_payload(msg + cur_len, MSG_BUF_LEN - cur_len, &new_len, IKE_PT_KEY_EXCHANGE, ike_ctx.ike_nonce_i);
-    if (error < 0) return error;
+    if (error < 0)
+        return error;
     cur_len += new_len;
 
     /* Construct Nonce payload */
-    chunk_t pubkey = malloc_chunk(96);// TODO: temp solution
+    chunk_t pubkey = malloc_chunk(96); // TODO: temp solution
     random_bytes((uint8_t *)pubkey.ptr, pubkey.len);
     error = build_key_exchange_payload(msg + cur_len, MSG_BUF_LEN - cur_len, &new_len, IKE_PT_NO_NEXT_PAYLOAD, ike_ctx.ike_dh, pubkey);
     free_chunk(&pubkey);
-    if (error < 0) return error;
+    if (error < 0)
+        return error;
     cur_len += new_len;
 
     /* Prepend header */
@@ -395,11 +401,15 @@ static int _parse_init_r(char *msg, size_t msg_len)
     ike_header_t *ike_hdr;
     size_t cur_len;
     ike_payload_type_t next_type;
-    if (msg_len < sizeof(ike_header_t)) {
+    if (msg_len < sizeof(ike_header_t))
+    {
+        puts("Message too short");
         return -EMSGSIZE;
     }
-    ike_hdr = (ike_header_t*)p;
-    if (ntohl(ike_hdr->length) != msg_len) {
+    ike_hdr = (ike_header_t *)p;
+    if (ntohl(ike_hdr->length) != msg_len)
+    {
+        puts("Message length mismatch");
         return -EMSGSIZE;
     }
     // TODO: more checks
@@ -408,15 +418,17 @@ static int _parse_init_r(char *msg, size_t msg_len)
     p += sizeof(ike_header_t);
     while (remaining_len > 0)
     {
-        switch (next_type) {
-            default:
-                if (process_unknown_payload(p, remaining_len, &cur_len, &next_type) < 0)
-                {
-                    return -1;
-                }
-                printf("Parsed unknown payload of size %u\n", cur_len);
-                remaining_len -= cur_len;
-                p += cur_len;
+        switch (next_type)
+        {
+        default:
+            if (process_unknown_payload(p, remaining_len, &cur_len, &next_type) < 0)
+            {
+                puts("Unknown payload parsing failed");
+                return -1;
+            }
+            printf("Parsed unknown payload of size %u\n", cur_len);
+            remaining_len -= cur_len;
+            p += cur_len;
         }
     }
     return 0;
