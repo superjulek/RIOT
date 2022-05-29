d90c744def4c13855041f021ddfdd2918d368146
Juliusz Neuman <01132997@pw.edu.pl>
2022-05-29 17:50:27 +0200

    Working encryption

--- sys/include/ike/ike_payloads.h
+++ sys/include/ike/ike_payloads.h
@@ -22,7 +22,8 @@
 #define IKE_PAYLOADS_H
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 #include "ike_structures.h"
@@ -30,25 +31,27 @@ extern "C" {
 
 #include "stddef.h"
 
-int build_sa_payload(char *start, size_t max_len, size_t *new_len, ike_payload_type_t next_payload, ike_protocol_id_t protocol,
-    ike_transform_encr_t encr, ike_transform_prf_t prf, ike_transform_integ_t integ,
-    ike_transform_dh_t dh, ike_transform_esn_t esn, size_t key_size, chunk_t spi);
+    int build_sa_payload(char *start, size_t max_len, size_t *new_len, ike_payload_type_t next_payload, ike_protocol_id_t protocol,
+                         ike_transform_encr_t encr, ike_transform_prf_t prf, ike_transform_integ_t integ,
+                         ike_transform_dh_t dh, ike_transform_esn_t esn, size_t key_size, chunk_t spi);
 
-int process_sa_payload(char *start, size_t max_len, size_t *cur_len, ike_payload_type_t *next_payload, ike_protocol_id_t *protocol,
-    ike_transform_encr_t *encr, ike_transform_prf_t *prf, ike_transform_integ_t *integ,
-    ike_transform_dh_t *dh, ike_transform_esn_t *esn, size_t *key_size, chunk_t *spi);
+    int process_sa_payload(char *start, size_t max_len, size_t *cur_len, ike_payload_type_t *next_payload, ike_protocol_id_t *protocol,
+                           ike_transform_encr_t *encr, ike_transform_prf_t *prf, ike_transform_integ_t *integ,
+                           ike_transform_dh_t *dh, ike_transform_esn_t *esn, size_t *key_size, chunk_t *spi);
 
-int build_nonce_payload(char *start, size_t max_len, size_t *new_len, ike_payload_type_t next_payload, chunk_t nonce);
+    int build_nonce_payload(char *start, size_t max_len, size_t *new_len, ike_payload_type_t next_payload, chunk_t nonce);
 
-int process_nonce_payload(char *start, size_t max_len, size_t *cur_len, ike_payload_type_t *next_payload, chunk_t *nonce);
+    int process_nonce_payload(char *start, size_t max_len, size_t *cur_len, ike_payload_type_t *next_payload, chunk_t *nonce);
 
-int build_key_exchange_payload(char *start, size_t max_len, size_t *new_len, ike_payload_type_t next_payload, ike_transform_dh_t dh, chunk_t key_data);
+    int build_key_exchange_payload(char *start, size_t max_len, size_t *new_len, ike_payload_type_t next_payload, ike_transform_dh_t dh, chunk_t key_data);
 
-int process_key_exchange_payload(char *start, size_t max_len, size_t *cur_len, ike_payload_type_t *next_payload, ike_transform_dh_t *dh, chunk_t *key_data);
+    int process_key_exchange_payload(char *start, size_t max_len, size_t *cur_len, ike_payload_type_t *next_payload, ike_transform_dh_t *dh, chunk_t *key_data);
 
-int process_generic_payload_header(char *start, size_t max_len, size_t *cur_len, ike_payload_type_t *next_payload);
+    int process_generic_payload_header(char *start, size_t max_len, size_t *cur_len, ike_payload_type_t *next_payload);
 
-int process_unknown_payload(char *start, size_t max_len, size_t *cur_len, ike_payload_type_t *next_payload);
+    int process_unknown_payload(char *start, size_t max_len, size_t *cur_len, ike_payload_type_t *next_payload);
+
+    int build_encrypted_payload(char *start, size_t max_len, size_t *new_len, ike_payload_type_t first_payload, chunk_t enc_data, chunk_t ekey, chunk_t akey);
 
 #ifdef __cplusplus
 }
