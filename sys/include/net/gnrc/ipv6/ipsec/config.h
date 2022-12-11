ad27c7a76fa92f76bc60bb381ea94181639828bf
Juliusz Neuman <superjulek@interia.pl>
2022-12-11 19:40:17 +0100

    WIP ESP out

--- sys/include/net/gnrc/ipv6/ipsec/config.h
+++ sys/include/net/gnrc/ipv6/ipsec/config.h
@@ -16,6 +16,31 @@ extern "C" {
 #define GNRC_IPV6_ESP_STACK_SIZE           (THREAD_STACKSIZE_DEFAULT)
 #endif
 
+/**
+ * @brief   Default priority for the ESP thread.
+ */
+#ifndef GNRC_IPV6_ESP_PRIO
+#define GNRC_IPV6_ESP_PRIO                 (THREAD_PRIORITY_MAIN - 4)
+#endif
+
+/**
+ * @brief   Default message queue size to use for the ESP thread (as
+ *          exponent of 2^n).
+ *
+ * As the queue size ALWAYS needs to be power of two, this option represents the
+ * exponent of 2^n, which will be used as the size of the queue.
+ */
+#ifndef CONFIG_GNRC_IPV6_ESP_MSG_QUEUE_SIZE_EXP
+#define CONFIG_GNRC_IPV6_ESP_MSG_QUEUE_SIZE_EXP   (3U)
+#endif
+
+/**
+ * @brief   Message queue size to use for the ESP thread.
+ */
+#ifndef GNRC_IPV6_ESP_MSG_QUEUE_SIZE
+#define GNRC_IPV6_ESP_MSG_QUEUE_SIZE    (1 << CONFIG_GNRC_IPV6_ESP_MSG_QUEUE_SIZE_EXP)
+#endif
+
 #ifdef __cplusplus
 }
 #endif
