a9fe175f52fa40689867bb27e566389d55ff2dc9
juliusz <superjulek@interia.pl>
2023-06-28 23:32:57 +0200

    Testing

--- examples/dtls-wolfssl-client/testing-commands.c
+++ examples/dtls-wolfssl-client/testing-commands.c
@@ -2,6 +2,8 @@
 #include "clients-cfg.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <xtimer.h>
 
 
 extern client_t udp_client;
@@ -69,6 +71,42 @@ error:
     return -1;
 }
 
+int loop_cmd(client_t *c, const char *addr, int loop_num)
+{
+    uint8_t buf[1] = {0};
+    uint8_t rcv_buf[1] = {0};
+
+    for (int i = 0; i < loop_num; ++i)
+    {
+        if (c->connect(addr, SERVER_PORT) < 0)
+            return -1;
+        xtimer_msleep(500);
+
+
+        printf("Sending 1 byte\n");
+        if (c->send((char*)buf, 1) < 0)
+            goto error;
+        if (c->receive((char*)rcv_buf, 1) != 1)
+        {
+            puts("Server returned different size");
+            goto error;
+        }
+        if (memcmp(buf, rcv_buf, 1) != 0)
+        {
+            puts("Server returned other data");
+            goto error;
+        }
+        printf("Success.\n");
+        c->close();
+        xtimer_msleep(500);
+    }
+    return 0;
+error:
+    printf("Error.\n");
+    c->close();
+    return -1;
+}
+
 int udp_client_cmd(int argc, char **argv)
 {
     if (argc != 2) {
@@ -99,4 +137,12 @@ int dtls_benchmark_cmd(int argc, char **argv)
         return -1;
     }
     return benchmark_cmd(&dtls_client, argv[1]);
+}
+
+int dtls_loop_cmd(int argc, char **argv)
+{
+    if (argc != 3) {
+        return -1;
+    }
+    return loop_cmd(&dtls_client, argv[1], atoi(argv[2]));
 }
